f59aa3ed41b17185cd4bc94a296071ebed69b1d9
kirran01 <Kirran-@live.com>
2023-10-23 19:18:17 -0400

    event routes

--- controllers/event.controller.js
+++ controllers/event.controller.js
@@ -14,6 +14,50 @@ const createEvent = async (req, res) => {
     res.send(err);
   }
 };
+const deleteEvent = async (req, res) => {
+  try {
+    const deletedEvent = await Event.deleteOne({ _id: req.params.id });
+    if (deletedEvent.deletedCount === 1) {
+      res.send("deleted");
+    } else {
+      res.send("something went wrong");
+    }
+  } catch (err) {
+    res.send(err);
+  }
+};
+const updateEvent = async (req, res) => {
+  try {
+    const { title, description } = req.body;
+    const updatedEvent = await Event.findByIdAndUpdate(
+      req.params.id,
+      { $set: { title: title, description: description } },
+      { new: true }
+    );
+
+    if (updatedEvent) {
+      res.json(updatedEvent);
+    } else {
+      res.send("event not found");
+    }
+  } catch (err) {
+    res.send(err);
+  }
+};
+const getEvents = async (req, res) => {
+  try {
+    const allEvents = await Event.find();
+    if (allEvents) {
+      res.send(allEvents);
+    }
+  } catch (err) {
+    res.send(err);
+  }
+};
+
 module.exports = {
   createEvent,
+  deleteEvent,
+  updateEvent,
+  getEvents,
 };
--- routes/event.routes.js
+++ routes/event.routes.js
@@ -1,8 +1,16 @@
 const express = require("express");
 const router = express.Router();
 
-const { createEvent } = require("../controllers/event.controller");
+const {
+  createEvent,
+  deleteEvent,
+  updateEvent,
+  getEvents,
+} = require("../controllers/event.controller");
 
+router.delete("/delete-event/:id", deleteEvent);
+router.get("/get-events", getEvents);
 router.post("/create-event", createEvent);
+router.put("/update-event/:id", updateEvent);
 
 module.exports = router;
